41c54482c0ccc11923c0fcb243513296988b6106
FlavioBarbosa <bsilvaflav@gmail.com>
2024-04-16 20:12:43 -0300

    Codigo atualizado

--- site-pizzaria/scripts/script.js
+++
@@ -1,9 +0,0 @@
-let menuContent = document.querySelector('.content');
-let menuToggle = menuContent.querySelector('.menu-toggle');
-let show = true;
-
-menuToggle.addEventListener('click', () => {
-    document.body.style.overflow = show ? 'hidden' : 'initial';
-    menuContent.classList.toggle('on', show);
-    show = !show;
-})
